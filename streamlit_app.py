cf3730e8378d69912dae3b2864e9f97c8ccb5345
foPhi <60519813+foPhi@users.noreply.github.com>
2023-08-21 14:54:45 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,6 +76,7 @@ if streamlit.button('Add a Fruit to the List'):
 
 ####
 
+streamlit.header("View Our Fruit List - Add Your Favorites!")
 # Snowflake-related functions
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
